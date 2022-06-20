2a26a6d79799ca85301b42a24e7e61da9389a505
sall166 <sallahmad059@gmail.com>
2022-06-20 14:35:37 +0200

    fix Home.Hero responsiveness

--- components/Home-components/Hero.jsx
+++ components/Home-components/Hero.jsx
@@ -3,14 +3,21 @@ import Section from '../Section';
 const Hero = () => {
     return ( 
         <section className={"hero has-background-danger-light is-fullheight "+ styles.sm_hero_bg}>
-            <div className="hero-body ">                
+            <div className={`hero-body ${styles.sm_hero_body}`}>                
                 <Section>
+                    <p className="subtitle">Hey there, I’m Robert</p>
                     <p className="title">
-                        Fullheight hero
+                        Freelance UI Designer & Web Developer
                     </p>
-                    <p className="subtitle">
-                        Fullheight subtitle
+                    <p>I help business grow by crafting amazing web experiences.
+                         If you’re looking for a designer and developer that likes to get stuff done, let’s talk.
                     </p>
+                    <br />
+                    <div className="buttons">
+                        <a href="" className="mr-4">sallmaxo@gmail.com</a>
+                        <a herf="">+0112 441 43 25</a>
+                    </div>
+                    <button className="button is-link">See my woek</button>
                 </Section>
             </div>                
         </section>
